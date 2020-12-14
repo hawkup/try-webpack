373beca01c0b1b2112f06a5e711f4e531f3413ee
hawkup <spiritsquare@gmail.com>
2020-12-14 22:32:38 +0700

    Add loading polyfills

---
+++ webpack4/shimming/04-loading-polyfills/webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    polyfills: './src/polyfills.js',
+    index: './src/index.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  }
+}
